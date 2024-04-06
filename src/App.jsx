cff0fa88b05aaa79883e24a26257c918c96ddeea
Mehedi Hasan <mhedih32@gmail.com>
2024-04-06 10:01:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold underline">Hello world!</h1>
+      <button className="btn btn-accent">Accent</button>
+    </>
+  )
+}
+
+export default App
